c41834378cb8a108da43a08fbdfd9e71881ea68a
hiralAadit <hir21st@gmail.com>
2022-12-15 21:02:01 +0000

    Framwork_mvn_homework_com-tutorialsninja_Hiral

---
+++ src/test/java/com/tutorialsninja/testsuite/LoginPageTest.java
@@ -0,0 +1,17 @@
+package com.tutorialsninja.testsuite;
+
+
+import com.tutorialsninja.testbase.BaseTest;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Jay Vaghani
+ */
+
+public class LoginPageTest extends BaseTest {
+
+
+}
